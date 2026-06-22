9e95d1a4a01089ecaa5ec20f5ba66b35973432ef
krunalchavhan26 <krunalchavhan5851@gmail.com>
2026-06-22 17:03:01 +0530

    solve: missing-number | arrays | easy | O(n) gaussian sum + notes: math tricks

---
+++ arrays/missing-number-easy.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int missingNumber(vector<int> &nums)
+    {
+        int n = nums.size();
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            actualSum += nums[i];
+        }
+
+        return expectedSum - actualSum;
+    }
+};
+
+int main()
+{
+    vector<int> nums = {3, 0, 1};
+    Solution sol;
+    cout << sol.missingNumber(nums) << endl; // 2
+    return 0;
+}
---
+++ notes/math-tricks.md
@@ -0,0 +1,19 @@
+# Math Tricks for DSA
+
+## Gaussian Sum Formula
+
+Sum of 0 to n = n \* (n + 1) / 2
+
+Example:
+0+1+2+3 = 6
+3\*(3+1)/2 = 6 ✅
+
+## When to use?
+
+- Missing Number problem
+- When you need sum of first n natural numbers
+- Avoid looping 0 to n just to get sum
+
+## Learned From
+
+- Missing Number (arrays/missing-number-easy.cpp)
